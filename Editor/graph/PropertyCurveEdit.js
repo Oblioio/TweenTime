327e49118f8c23a3c55b12f868d059218e0afb4a
Scott Agrimson <scott@Scotts-MacBook-Pro.local>
2021-03-01 10:02:11 -0800

    remove logs

--- Editor/graph/PropertyCurveEdit.js
+++ Editor/graph/PropertyCurveEdit.js
@@ -6,7 +6,6 @@ import Utils from '../../Utils';
 const MAX_HEIGHT = 190;
 
 function getVal() {
-  console.log('TYPEOF', this.val);
   switch(typeof this.val) {
     case 'number':
       return this.val;
@@ -24,7 +23,6 @@ function getVal() {
         }
       }
 
-      console.log('TOTAL', total, keys);
       return total / numvals;
     default:
       console.log('weird case', this.val);
@@ -41,7 +39,6 @@ export default class PropertyCurveEdit {
   }
 
   normalizeVal(val, min, max, min2, max2) {
-    console.log('NORMALIZE', val, min, max, min2, max2);
     if (min === min2 && max === max2) {
       return val;
     }
@@ -53,7 +50,6 @@ export default class PropertyCurveEdit {
 
   // Get bezier point from easing (0 to 1) and previous and next point.
   bezierPoint(pt, prev, next) {
-    console.log('BEZIER POINT', pt, prev, next);
     const x = this.normalizeVal(pt.x, 0, 1, prev.x, next.x);
     const y = this.normalizeVal(pt.y, 0, 1, prev.y, next.y);
     return {x, y};
@@ -98,9 +94,6 @@ export default class PropertyCurveEdit {
       const x = this.timeline.x(key.time * 1000);
       const y = this.normalizeVal(getVal.call(key), d._min, d._max, 0, MAX_HEIGHT);
       d._curvePoints.push({x, y, ease: key.ease, id: `curve1-${key._id}`, _key: key});
-      if (isNaN(y)) {
-        console.log('ISNAN', getVal.call(key), d._min, d._max, 0, MAX_HEIGHT);
-      }
     });
 
     // Control points, grouped by point + handle.
@@ -112,7 +105,6 @@ export default class PropertyCurveEdit {
       // If non number points return an empty path.
       if (isNaN(pt.x) || isNaN(pt.y)) {
         invalid = true;
-        // console.log('THIS IS ALSO INVALID', pt);
       }
       d._curvePointsBezier.push({x: pt.x, y: pt.y, ease: pt.ease, _key: pt._key});
       if (next) {
@@ -137,7 +129,6 @@ export default class PropertyCurveEdit {
             isNaN(p1.x) || isNaN(p1.y) ||
             isNaN(p2.x) || isNaN(p2.y)) {
           invalid = true;
-          // console.log('THIS IS INVALID', next, p1, p2, d);
         }
         d._controlPoints.push({point: pt, handle: p1, id: `${pt._key._id}-a`});
         d._controlPoints.push({point: next, handle: p2, id: `${pt._key._id}-b`});
